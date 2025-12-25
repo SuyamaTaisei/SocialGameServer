4a416ccfd449f30696fd78848e69117202f64a33
SuyamaTaisei <artistplain@gmail.com>
2025-12-25 22:09:20 +0900

    [#11231] アイテム所持数の処理追加
    ・アイテム所持数に上限値を実装

--- app/Services/ItemAddService.php
+++ app/Services/ItemAddService.php
@@ -12,13 +12,31 @@ class ItemAddService
         //item_idを取得
         $existItem = ItemInstance::where('manage_id', $manageId)->where('item_id', $itemId)->first();
 
+        //現在のアイテム数を取得、何もアイテムが無ければ0を取得
+        $currentAmount = $existItem?->amount ?? 0;
+
+        //上限値を超えた場合は何もしない
+        if ($currentAmount >= config('common.MAX_ITEM_INSTANCE'))
+        {
+            return;
+        }
+
+        //上限値に応じた追加アイテム数の取得
+        $addValue = min($amountValue, config('common.MAX_ITEM_INSTANCE') - $currentAmount);
+
+        //amountValueが0、上限値 - 最高所持数の場合は何もしない
+        if ($addValue <= 0)
+        {
+            return;
+        }
+
         //初めてアイテムをもらった場合
         if ($existItem === null)
         {
             ItemInstance::create([
                 'manage_id' => $manageId,
                 'item_id'   => $itemId,
-                'amount'    => $amountValue,
+                'amount'    => $addValue,
             ]);
         }
 
@@ -26,7 +44,7 @@ class ItemAddService
         else
         {
             $existItem->update([
-                'amount' => $existItem->amount + $amountValue,
+                'amount' => $currentAmount + $addValue,
             ]);
         }
     }
