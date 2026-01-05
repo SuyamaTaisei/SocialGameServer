5f012e82bd473ce83539987b0fd1a5f87ac9f075
SuyamaTaisei <artistplain@gmail.com>
2026-01-05 16:36:27 +0900

    [#11240] スタミナ用APIのルート受け付け変更
    ・スタミナ用のAPIをgetからpostに変更

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ Route::post('/get_master_data', App\Http\Controllers\MasterDataController::class
 Route::post('/payment', App\Http\Controllers\PaymentController::class); //ショップ購入API
 Route::post('/gacha_execute', App\Http\Controllers\GachaExecuteController::class); //ガチャ実行API
 Route::post('/enhance_character', App\Http\Controllers\EnhanceController::class); //キャラ強化API
-Route::get('/stamina_decrease', App\Http\Controllers\StaminaDecreaseController::class); //スタミナ消費API
-Route::get('/stamina_increase', App\Http\Controllers\StaminaIncreaseController::class); //スタミナ回復API
-Route::get('/stamina_auto_increase', App\Http\Controllers\StaminaAutoIncreaseController::class); //スタミナ自然回復API
+Route::post('/stamina_decrease', App\Http\Controllers\StaminaDecreaseController::class); //スタミナ消費API
+Route::post('/stamina_increase', App\Http\Controllers\StaminaIncreaseController::class); //スタミナ回復API
+Route::post('/stamina_auto_increase', App\Http\Controllers\StaminaAutoIncreaseController::class); //スタミナ自然回復API
