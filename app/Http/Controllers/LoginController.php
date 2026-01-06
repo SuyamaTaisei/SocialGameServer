ede8cb97f845ad8abb48d64496e959412382a005
SuyamaTaisei <artistplain@gmail.com>
2026-01-06 10:33:05 +0900

    [#11241] スタミナ差分サービスの実装
    ・前回ログイン日と現在ログイン日の差分を計算して、分単位でその差分の数だけスタミナを更新する処理を実装

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\GachaLog;
+
+use App\Services\StaminaDiffService;
+
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,20 +14,37 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, StaminaDiffService $staminaDiffService)
     {
         $result = config('common.RESPONSE_FAILED');
-        $response['result'] = config('common.RESPONSE_SUCCESS');
 
         //ユーザー情報取得
         $userData = User::where('id', $request->id)->first();
 
-        DB::transaction(function() use (&$result, $userData)
+        //ユーザー情報がなければエラーを返して何もしない
+        if (!$userData)
         {
-            //最終ログイン時間更新
-            $result = User::where('manage_id', $userData->manage_id)->update([
-                'last_login' => Carbon::now()->format('Y-m-d H:i:s'),
+            $result = config('common.RESPONSE_FAILED');
+            return;
+        }
+
+        //現在ログイン時刻
+        $currentLogin = Carbon::now();
+        //最終ログイン時刻
+        $lastLogin = Carbon::parse($userData->last_login);
+
+        //スタミナ差分計算サービス
+        $staminaDiffData = $staminaDiffService->StaminaDiff($userData, $lastLogin, $currentLogin);
+
+        DB::transaction(function() use (&$result, $userData, $staminaDiffData, $currentLogin)
+        {
+            $userData->update([
+                'last_login' => $currentLogin->format('Y-m-d H:i:s'),
+                'last_stamina' => $staminaDiffData,
+                'stamina_updated' => $currentLogin->format('Y-m-d H:i:s'),
             ]);
+
+            $result = config('common.RESPONSE_SUCCESS');
         });
 
         switch ($result)
