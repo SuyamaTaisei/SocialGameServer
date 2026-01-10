3ff38c0745854f8045b791adb4a9e03ed2b2ed4d
SuyamaTaisei <artistplain@gmail.com>
2026-01-10 19:20:45 +0900

    [#11286] ショップAPIの名前変更
    ・ショップ用APIのファイル名およびクラス名を適切な名前に変更

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::post('/login', App\Http\Controllers\LoginController::class); //ログイ�
 Route::post('/home', App\Http\Controllers\HomeController::class); //ホーム情報取得API
 Route::post('/check_master_data', App\Http\Controllers\MasterCheckController::class); //マスタデータチェック取得API
 Route::post('/get_master_data', App\Http\Controllers\MasterDataController::class); //マスタデータ取得API
-Route::post('/payment', App\Http\Controllers\PaymentController::class); //ショップ購入API
+Route::post('/payment', App\Http\Controllers\ShopController::class); //ショップ購入API
 Route::post('/gacha_execute', App\Http\Controllers\GachaExecuteController::class); //ガチャ実行API
 Route::post('/enhance_character', App\Http\Controllers\EnhanceController::class); //キャラ強化API
 Route::post('/stamina_decrease', App\Http\Controllers\StaminaDecreaseController::class); //スタミナ消費API
