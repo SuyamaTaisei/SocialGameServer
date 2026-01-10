394d3d6c362c3b6a8d33c0918669cf045ec6a51f
SuyamaTaisei <artistplain@gmail.com>
2026-01-10 22:41:07 +0900

    [#11287] スタミナ差分処理の修正
    ・スタミナ差分計算に使用するカラムを最終ログイン用カラムから、スタミナ更新用カラムに変更
    ・一度試して不要になった、downにロールバック済みのマイグレーション

--- app/Services/StaminaDiffService.php
+++ app/Services/StaminaDiffService.php
@@ -8,10 +8,10 @@ use Carbon\Carbon;
 //スタミナ差分計算サービス
 class StaminaDiffService
 {
-    public function StaminaDiff(User $userData, Carbon $lastLogin, Carbon $currentLogin)
+    public function StaminaDiff(User $userData, Carbon $lastDateTime, Carbon $currentLogin)
     {
         //最終ログインから現在ログインまでの差分を分単位で取得
-        $diffValue = (int)$lastLogin->diffInMinutes($currentLogin, true);
+        $diffValue = (int)$lastDateTime->diffInMinutes($currentLogin, true);
 
         //差分の分数だけスタミナを増やす
         $addDiff = $diffValue * config('common.STAMINA_INCREASE_AUTO_VALUE');
