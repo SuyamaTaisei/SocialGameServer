be93fa24eb2fa025055da3890a3bec7009d3bb2e
SuyamaTaisei <artistplain@gmail.com>
2026-01-15 16:15:15 +0900

    [#11368] 各種APIの修正
    ・ホームAPIとログインAPIの余分なコードを削減

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,16 +16,13 @@ class LoginController extends Controller
 {
     public function __invoke(Request $request, StaminaDiffService $staminaDiffService)
     {
-        $result = config('common.RESPONSE_FAILED');
-
         //ユーザー情報取得
         $userData = User::where('id', $request->id)->first();
 
         //ユーザー情報がなければエラーを返して何もしない
         if (!$userData)
         {
-            $result = config('common.RESPONSE_FAILED');
-            return;
+            return response()->json(['errcode' => config('common.RESPONSE_ERROR')]);
         }
 
         //現在ログイン時刻
@@ -36,32 +33,21 @@ class LoginController extends Controller
         //スタミナ差分計算サービス
         $staminaDiffData = $staminaDiffService->StaminaDiff($userData, $lastStaminaUpdated, $currentLogin);
 
-        DB::transaction(function() use (&$result, $userData, $staminaDiffData, $currentLogin)
+        DB::transaction(function() use ($userData, $staminaDiffData, $currentLogin)
         {
             $userData->update([
                 'last_login' => $currentLogin->format('Y-m-d H:i:s'),
                 'last_stamina' => $staminaDiffData,
                 'stamina_updated' => $currentLogin->format('Y-m-d H:i:s'),
             ]);
-
-            $result = config('common.RESPONSE_SUCCESS');
         });
 
-        switch ($result)
-        {
-            //エラー時
-            case config('common.RESPONSE_FAILED'):
-                $response['result'] = config('common.RESPONSE_ERROR');
-                break;
-            //ログイン時に必要な情報を取得
-            case config('common.RESPONSE_SUCCESS'):
-                $response = 
-                [
-                    'users' => User::where('manage_id', $userData->manage_id)->first(),
-                    'gacha_logs' => GachaLog::where('manage_id', $userData->manage_id)->get(),
-                ];
-                break;
-        }
+        //レスポンスデータ
+        $response = 
+        [
+            'users' => User::where('manage_id', $userData->manage_id)->first(),
+            'gacha_logs' => GachaLog::where('manage_id', $userData->manage_id)->get(),
+        ];
 
         Auth::login($userData);
 
