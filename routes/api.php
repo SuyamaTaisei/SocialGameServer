a781ebda9dfdd088fa1bb90cd3f43749fbb88d49
SuyamaTaisei <artistplain@gmail.com>
2026-02-08 20:58:24 +0900

    [#11511] ルートファイルの変更
    ・必要なルートをgetからpostに変更

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ Route::post('/home', App\Http\Controllers\HomeController::class); //ホーム情
 Route::post('/check_master_data', App\Http\Controllers\MasterCheckController::class); //マスタデータチェック取得API
 Route::post('/get_master_data', App\Http\Controllers\MasterDataController::class); //マスタデータ取得API
 Route::post('/payment', App\Http\Controllers\ShopController::class); //ショップ購入API
-Route::get('/gacha_execute', App\Http\Controllers\GachaExecuteController::class); //ガチャ実行API
-Route::get('/enhance_character', App\Http\Controllers\CharacterEnhanceController::class); //キャラ強化API
-Route::get('/stamina_decrease', App\Http\Controllers\StaminaDecreaseController::class); //スタミナ消費API
-Route::get('/stamina_increase', App\Http\Controllers\StaminaIncreaseController::class); //スタミナ回復API
+Route::post('/gacha_execute', App\Http\Controllers\GachaExecuteController::class); //ガチャ実行API
+Route::post('/enhance_character', App\Http\Controllers\CharacterEnhanceController::class); //キャラ強化API
+Route::post('/stamina_decrease', App\Http\Controllers\StaminaDecreaseController::class); //スタミナ消費API
+Route::post('/stamina_increase', App\Http\Controllers\StaminaIncreaseController::class); //スタミナ回復API
 Route::post('/stamina_auto_increase', App\Http\Controllers\StaminaAutoIncreaseController::class); //スタミナ自然回復API
-Route::get('/present_received', App\Http\Controllers\PresentReceivedController::class); //プレゼント受け取りAPI
+Route::post('/present_received', App\Http\Controllers\PresentReceivedController::class); //プレゼント受け取りAPI
 Route::post('/present_delete', App\Http\Controllers\PresentDeleteController::class); //プレゼント削除API
-Route::get('/mission_received', App\Http\Controllers\MissionReceivedController::class); //ミッション受け取りAPI
+Route::post('/mission_received', App\Http\Controllers\MissionReceivedController::class); //ミッション受け取りAPI
