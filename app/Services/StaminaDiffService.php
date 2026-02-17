6f472c993f0dc5e1cbae49f5548aaaa9ad2a4099
SuyamaTaisei <artistplain@gmail.com>
2026-02-17 20:49:26 +0900

    [#11584] スタミナ最大値の修正
    ・スタミナの差分計算および自然回復を最大値(100)になるように修正

--- app/Services/StaminaDiffService.php
+++ app/Services/StaminaDiffService.php
@@ -10,14 +10,21 @@ class StaminaDiffService
 {
     public function StaminaDiff(User $userData, Carbon $lastDateTime, Carbon $currentLogin)
     {
-        //最終ログインから現在ログインまでの差分を分単位で取得
-        $diffValue = (int)$lastDateTime->diffInMinutes($currentLogin, true);
-
-        //差分の分数だけスタミナを増やす
-        $addDiff = $diffValue * config('common.STAMINA_INCREASE_AUTO_VALUE');
-
-        //現在のスタミナに差分の数だけスタミナを足す。現在が最大値であれば常に最大値を返す
-        $addStamina = min($userData->last_stamina + $addDiff, config('common.STAMINA_MAX_VALUE'));
+        if ($userData->last_stamina <= config('common.STAMINA_MOST_VALUE'))
+        {
+            //最終ログインから現在ログインまでの差分を分単位で取得
+            $diffValue = (int)$lastDateTime->diffInMinutes($currentLogin, true);
+    
+            //差分の分数だけスタミナを増やす
+            $addDiff = $diffValue * config('common.STAMINA_INCREASE_AUTO_VALUE');
+    
+            //現在のスタミナに差分の数だけスタミナを足す。現在が最大値であれば常に最大値を返す
+            $addStamina = min($userData->last_stamina + $addDiff, config('common.STAMINA_MOST_VALUE'));
+        }
+        else
+        {
+            $addStamina = $userData->last_stamina;
+        }
 
         return $addStamina;
     }
